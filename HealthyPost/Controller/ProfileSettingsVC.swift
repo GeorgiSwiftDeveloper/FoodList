06a1187235916e6face5dd45631b6cdfb5a8d468
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-08-20 16:30:23 -0700

    UIImagePickerDelegate

--- HealthyPost/Controller/ProfileSettingsVC.swift
+++ HealthyPost/Controller/ProfileSettingsVC.swift
@@ -33,8 +33,8 @@ class ProfileSettingsVC: UIViewController,UIImagePickerControllerDelegate,UINavi
 //            userLocationTextField.text = userLocation as? String
 //        }
 //
-//         let imageTapp = UITapGestureRecognizer(target: self, action: #selector(userImageTapped))
-//         self.userAddImage.addGestureRecognizer(imageTapp)
+         let imageTapp = UITapGestureRecognizer(target: self, action: #selector(userImageTapped))
+         self.userAddImage.addGestureRecognizer(imageTapp)
     }
     
 //    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -61,44 +61,45 @@ class ProfileSettingsVC: UIViewController,UIImagePickerControllerDelegate,UINavi
     
 //
 //
-//    @objc func userImageTapped(_ sender: UIGestureRecognizer){
-//        let imagepickerController = UIImagePickerController()
-//        imagepickerController.delegate = self
-//        let imageAlert = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-//        imageAlert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
-//            if UIImagePickerController.isSourceTypeAvailable(.camera) {
-//                imagepickerController.sourceType = .camera
-//                self.present(imagepickerController, animated: true, completion: nil)
-//            }else {
-//                print("camera not available")
-//            }
-//        }))
-//        imageAlert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
-//            imagepickerController.sourceType = .photoLibrary
-//            self.present(imagepickerController, animated: true, completion: nil)
-//        }))
-//        imageAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
-//        }))
-//        self.present(imageAlert, animated: true, completion: nil)
-//    }
-//
-//
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//        var selectedImage: UIImage?
-//        if let editedImage = info[.editedImage] as? UIImage {
-//            selectedImage = editedImage
-//            picker.dismiss(animated: true, completion: nil)
-//        } else if let originalImage = info[.originalImage] as? UIImage {
-//            selectedImage = originalImage
-//            userAddImage.image = selectedImage
-//            let dataImage = selectedImage?.pngData()
-//            UserDefaults.standard.set(dataImage, forKey: "image")
-//            picker.dismiss(animated: true, completion: nil)
-//        }
-//    }
-//
-    @IBAction func backToTheMainVC(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+    @objc func userImageTapped(_ sender: UIGestureRecognizer){
+        let imagepickerController = UIImagePickerController()
+        imagepickerController.delegate = self
+        let imageAlert = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        imageAlert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagepickerController.sourceType = .camera
+                self.present(imagepickerController, animated: true, completion: nil)
+            }else {
+                print("camera not available")
+            }
+        }))
+        imageAlert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+            imagepickerController.sourceType = .photoLibrary
+            self.present(imagepickerController, animated: true, completion: nil)
+        }))
+        imageAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+        }))
+        self.present(imageAlert, animated: true, completion: nil)
     }
+
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var selectedImage: UIImage?
+        if let editedImage = info[.editedImage] as? UIImage {
+            selectedImage = editedImage
+            picker.dismiss(animated: true, completion: nil)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            selectedImage = originalImage
+            userAddImage.image = selectedImage
+            let dataImage = selectedImage?.pngData()
+            UserDefaults.standard.set(dataImage, forKey: "image")
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func backBtnPress(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+    }
+    
     
 }
